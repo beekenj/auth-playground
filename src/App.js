d314247ac3e3847fdcb7f310bdc0b3e67fe6fb93
Jack Beeken <beekenj@gmail.com>
2023-05-25 17:46:49 -0600

    added .env file to process and .gitignore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  // console.log(`${process.env.REACT_APP_API_KEY}`)
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
